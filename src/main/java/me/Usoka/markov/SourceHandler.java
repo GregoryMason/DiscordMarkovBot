ba310a4b4b09e573b68658ef14fa9f2fec0ee698
GregoryMason <GregoryMason@users.noreply.github.com>
2018-04-21 10:31:28 +1200

    Update comments in Handler interfaces

--- src/main/java/me/Usoka/markov/SourceHandler.java
+++ src/main/java/me/Usoka/markov/SourceHandler.java
@@ -20,8 +20,8 @@ public interface SourceHandler {
 	boolean updateMessage(Message message);
 
 	/**
-	 * Update multiple messages in the source.
-	 * Will add the messages if any don't exist
+	 * Update multiple messages in the source at once.
+	 * Will add any messages which didn't previously exist
 	 * @param messages List of messages that should be updated
 	 * @return if updating messages was successful
 	 */
@@ -29,24 +29,47 @@ public interface SourceHandler {
 
 	/**
 	 * Remove a specified message from the source
-	 * @param messageID ID of message to delete
-	 * @return if it was successfully deleted
+	 * @param messageID ID (as String) of message to delete
+	 * @return if it was deleted successfully
 	 */
 	boolean deleteMessage(String messageID);
 
 	/**
-	 * Identify if a message is already saved, based on ID
-	 * @param messageID ID of the message
+	 * Identify if a message, by given ID, already existing in the source
+	 * @param messageID ID (as String) of message to check for
 	 * @return if the message exist
+	 * @throws Exception if there is an error reading from the source
 	 */
 	boolean containsMessageID(String messageID) throws Exception;
 
+	/**
+	 * Saves a specified user to the source data.
+	 * Will also replace an existing user if ID matches
+	 * @param user User to save to source
+	 * @return if the user was saved successfully
+	 */
 	boolean saveUser(User user);
 
-
+	/**
+	 * Checks if a user, by given ID, exists in the source data
+	 * @param userID user ID (as String) to check for
+	 * @return if the source data contains a user with matching ID
+	 * @throws Exception if there is an error reading from the source
+	 */
 	boolean containsUserID(String userID) throws Exception;
 
+	/**
+	 * Gets the String ID of the most recent message in the source data
+	 * @return the ID of the most recent message (as String)
+	 * @throws Exception if there is an error reading from the source
+	 */
 	String getMostRecentMessageID() throws Exception;
 
+	/**
+	 * Counts the number of messages in the source data for a specified user
+	 * @param user specified user to count the messages for
+	 * @return number of messages which the user has sent
+	 * @throws Exception if there is an error reading from the source
+	 */
 	int countMessagesFromUser(User user) throws Exception;
 }
