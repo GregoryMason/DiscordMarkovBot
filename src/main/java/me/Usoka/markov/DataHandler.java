9f07af7142f186fe6078fa5959ebcbe9e396950a
Usoka <27248545+Usoka@users.noreply.github.com>
2018-04-21 10:31:28 +1200

    Update comments in Handler interfaces

--- src/main/java/me/Usoka/markov/DataHandler.java
+++ src/main/java/me/Usoka/markov/DataHandler.java
@@ -5,25 +5,63 @@ import java.util.List;
 public interface DataHandler {
 
 	/**
-	 * @return how many unique words are in the lexicon built from all users
+	 * Gets how many unique words are in the complete lexicon from all users
+	 * @return number of unique words
 	 */
 	int getLexiconSize();
 
 	/**
-	 * @param user specified user
-	 * @return how many unique words are in the lexicon for the specified user
+	 * Gets how many unique words are in the lexicon for a specified user
+	 * @param user specified user for lexicon
+	 * @return number of unique words
 	 */
 	int getLexiconSizeFor(User user);
 
+	/**
+	 * Gets the frequency for a specified word, and a specified user. (how
+	 * many times the user has said that specific word)
+	 * @param word specified word to find frequency of
+	 * @param user specified user to find the frequency for
+	 * @return number of times the user has said the word
+	 */
 	int getFrequencyOf(String word, User user);
 
+	/**
+	 * Gets how many times any use has said a specified word
+	 * @param word specified word to find the frequency of
+	 * @return total number of times any user has said the word
+	 */
 	int getFrequencyAllOf(String word);
 
+	/**
+	 * Gets the complete list of unique words said by all users
+	 * @return <code>List</code> containing all words
+	 */
 	List<String> getLexiconAll();
 
+	/**
+	 * Gets the complete list of unique words said for a specific user
+	 * @param user specified user to get the lexicon for
+	 * @return <code>List</code> containing all words from that user
+	 */
 	List<String> getLexiconFor(User user);
 
+	/**
+	 * Gets the words that are linked to from a given word, for a specified
+	 * user, in the markov data
+	 * @param user specified user to get the markov links for
+	 * @param word specified word to get links from
+	 * @return <code>List</code> containing all linked words
+	 * TODO update this to a HashMap of linkedWord->frequency
+	 */
 	List<String> getLinksFor(User user, String word);
 
+	/**
+	 * Gets the words that are linked to from a given one, for all
+	 * users in the markov data
+	 * @param word specified word to get links from
+	 * @return <code>List</code> containing all linked words
+	 * TODO update this to a HashMap of linkedWord->frequency
+	 */
 	List <String> getLinksAll(String word);
 }
