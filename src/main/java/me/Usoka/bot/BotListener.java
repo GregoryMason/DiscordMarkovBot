73b1c508df02536dc37b4c82814b1baa4c795bce
Usoka <27248545+Usoka@users.noreply.github.com>
2018-07-11 16:42:08 +1200

    Remove unused import

--- src/main/java/me/Usoka/bot/BotListener.java
+++ src/main/java/me/Usoka/bot/BotListener.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 /**
  * TODO split into multiple classes, this has gotten too big/complicates <p/>
